5745fb435887e19539680927d85e871cd05a02b1
ishanyadav13290 <ishanyadav13290@gmail.com>
2023-01-23 22:14:00 +0530

    other bugs in navbar fixed

--- src/components/navbar/nav2.js
+++ src/components/navbar/nav2.js
@@ -10,14 +10,14 @@ export default function Nav2() {
 
     return (
         <Flex id="mid" h="100px" justify="center" align="center" className={styles.fill}>
-            <Flex align="center" justify="space-between" w="1200px">
+            <Flex align="center" justify="space-between" w="95%">
             <DrawerExample />
-                <NavLink to="/"><Image src="https://www.mirraw.com/assets/logo-red.png" w={"100%"}/></NavLink>
+                <NavLink to="/"><Box><Image src="https://www.mirraw.com/assets/logo-red.png" w={["50%","100%"]} minW={["50%","100%"]} m="0 20%" /></Box></NavLink>
                 <Flex border="1px solid black" w="50%" display={["none", "none", "flex"]}>
                     <Input border="none" fontStyle="italic" placeholder="Search Something..." borderRadius="0" w="90%"></Input>
                     <Button _hover={{background:"#670b19"}} bg="#670b19" borderRadius="0" w="10%"><LordIconSearch /></Button>
                 </Flex>
-                <Flex display={["flex", "flex", "none"]}>
+                <Flex display={["flex", "flex", "none"]} ml="5%">
 
                 <CartLog />
 
