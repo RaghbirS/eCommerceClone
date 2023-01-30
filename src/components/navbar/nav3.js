015cc82fe3d1e4f7721c18937facb7c249c979a8
RaghbirS <rkgaming0203@gmail.com>
2023-01-30 14:15:01 +0530

    minor search bug fixed

--- src/components/navbar/nav3.js
+++ src/components/navbar/nav3.js
@@ -12,53 +12,59 @@ import Navdrop8 from "./navDrops/navDrop8";
 import Navdrop9 from "./navDrops/navDrop9";
 import Navdrop10 from "./navDrops/navDrop10";
 import Navdrop11 from "./navDrops/navDrop11";
+import { useContext } from "react";
+import { AuthContext } from "../AuthContext/context";
 export default function Nav3() {
+    const {setActiveSearch} = useContext(AuthContext);
+    function setSearchFalse(){
+        setActiveSearch(false)
+    }
     return (
         <Flex zIndex="1" id="bot" style={{ boxShadow: "rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px" }} className={styles.nav3} justify="center" w="100vw" h="40px">
             <Flex className={styles.giveSty} position="relative" align="center">
                 {/* <Box className="navHove1" > */}
                 <Box>
-                    <NavLink to="/products/saree">Sarees</NavLink>
+                    <NavLink onClick={setSearchFalse} to="/products/saree">Sarees</NavLink>
                     <Navdrop1 />
                 </Box>
                 {/* <Box  className="navHove2" > */}
                 <Box>
-                    <NavLink  to="/products/Lehengas">Lehengas</NavLink>
+                    <NavLink onClick={setSearchFalse}  to="/products/Lehengas">Lehengas</NavLink>
                     <Navdrop2 />
                 </Box>
                 {/* <Box className="navHove3" > */}
                 <Box>
-                    <NavLink _hover={{ borderBottom: "2px solid #670b19" }} to="/products/salwaarKameez">SalwarKameez</NavLink>
+                    <NavLink onClick={setSearchFalse} _hover={{ borderBottom: "2px solid #670b19" }} to="/products/salwaarKameez">SalwarKameez</NavLink>
                     <Navdrop3 />
                 </Box>
                 {/* <Box className="navHove4" > */}
                 <Box>
-                    <NavLink _hover={{ borderBottom: "2px solid #670b19" }} to="/products/kurtis">Kurtis</NavLink>
+                    <NavLink onClick={setSearchFalse} _hover={{ borderBottom: "2px solid #670b19" }} to="/products/kurtis">Kurtis</NavLink>
                     <Navdrop4 />
                 </Box>
                 {/* <Box className="navHove5" > */}
                 <Box>
-                    <NavLink _hover={{ borderBottom: "2px solid #670b19" }} to="/products/jewellary">Jewellery</NavLink>
+                    <NavLink onClick={setSearchFalse} _hover={{ borderBottom: "2px solid #670b19" }} to="/products/jewellary">Jewellery</NavLink>
                     <Navdrop5 />
                 </Box>
                 {/* <Box className="navHove6" > */}
                 <Box>
-                    <NavLink _hover={{ borderBottom: "2px solid #670b19" }} to="/products/kids">Kids</NavLink>
+                    <NavLink onClick={setSearchFalse} _hover={{ borderBottom: "2px solid #670b19" }} to="/products/kids">Kids</NavLink>
                     <Navdrop6 />
                 </Box>
                 {/* <Box className="navHove7" > */}
                 <Box>
-                    <NavLink _hover={{ borderBottom: "2px solid #670b19" }} to="/products/mens">Men</NavLink>
+                    <NavLink onClick={setSearchFalse} _hover={{ borderBottom: "2px solid #670b19" }} to="/products/mens">Men</NavLink>
                     <Navdrop7 />
                 </Box>
                 {/* <Box className="navHove8" > */}
                 <Box>
-                    <NavLink _hover={{ borderBottom: "2px solid #670b19" }} to="/products/homeLiving">Home & Living</NavLink>
+                    <NavLink onClick={setSearchFalse} _hover={{ borderBottom: "2px solid #670b19" }} to="/products/homeLiving">Home & Living</NavLink>
                     <Navdrop8 />
                 </Box>
                 {/* <Box className="navHove10" > */}
                 <Box>
-                    <NavLink _hover={{ borderBottom: "2px solid #670b19" }} to="/products/spiritual">Spiritual</NavLink>
+                    <NavLink onClick={setSearchFalse} _hover={{ borderBottom: "2px solid #670b19" }} to="/products/spiritual">Spiritual</NavLink>
                     <Navdrop10 />
                 </Box>             
             </Flex>
